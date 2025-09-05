2673abcf1b30c32ce0a0df056fe6592049827883
marty <baldiperez@gmail.com>
2025-09-06 00:35:36 +0200

    remove placeholder values from terraform backend

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,11 +1,6 @@
 terraform {
   required_version = ">= 1.0.0"
   backend "s3" {
-    bucket         = "your-bucket-name"
-    key            = "path/to/terraform.tfstate"
-    region         = "your-region"
-    dynamodb_table = "your-lock-table" # optional, for locking
-    encrypt        = true
   }
 }
 
