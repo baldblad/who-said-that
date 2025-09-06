cb31ec83dee23f80d9c2c7cb34ee54a97f498ff1
marty <baldiperez@gmail.com>
2025-09-06 13:20:42 +0200

    refactor cloudfront distribution

--- terraform/static_site.tf
+++ terraform/static_site.tf
@@ -37,7 +37,7 @@ data "aws_iam_policy_document" "cloudfront" {
 resource "aws_cloudfront_distribution" "ui_cdn" {
   origin {
     domain_name = aws_s3_bucket.ui_bucket.bucket_regional_domain_name
-    origin_id   = "uiS3Origin"
+    origin_id   = "static-web.${aws_s3_bucket.ui_bucket.bucket}-origin"
   }
 
   enabled             = true
@@ -47,8 +47,19 @@ resource "aws_cloudfront_distribution" "ui_cdn" {
   default_cache_behavior {
     allowed_methods  = ["GET", "HEAD"]
     cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "uiS3Origin"
-    viewer_protocol_policy = "redirect-to-https"
+    target_origin_id = "static-web.${aws_s3_bucket.ui_bucket.bucket}-origin"
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
   }
 
   restrictions {
@@ -60,4 +71,6 @@ resource "aws_cloudfront_distribution" "ui_cdn" {
   viewer_certificate {
     cloudfront_default_certificate = true
   }
+
+	tags = []
 }
